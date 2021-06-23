d87096095e2558894943b660c1f35d74df3d84d4
Kevin Degeling <contact@kevindegeling.nl>
2021-06-23 17:20:29 +0200

    Rebuild directly from proper branch

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@ clear
 
 # Debugging setup starts here
 set -eu
-flatpak install --system flathub org.freedesktop.Platform//20.08
-flatpak install --system flathub org.freedesktop.Sdk//20.08
 flatpak-builder --repo=testing-repo --force-clean build-dir org.dhewm3.Dhewm3.yaml
 flatpak --user remote-add --if-not-exists --no-gpg-verify d3-testing-repo testing-repo
 flatpak --user install d3-testing-repo org.dhewm3.Dhewm3 -y
